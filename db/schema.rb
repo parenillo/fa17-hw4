8c0fcf81fb7fd415e268357ca6214fe168ca4b34
Patricia Arenillo <parenillo@berkeley.edu>
2017-10-05 22:43:19 -0700

    Todo done

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171006053150) do
+ActiveRecord::Schema.define(version: 20171006054038) do
 
   create_table "cats", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "todos", force: :cascade do |t|
+    t.string "tasks"
+    t.boolean "finished"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
